4b7dcb33aa7909d4d8aefaf47e23a7ec9c47a6b0
Wahyu Hidayat <wahyuwhyhidayat@gmail.com>
2023-05-19 22:03:10 +0700

    Task 1 Answer

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,13 @@ namespace Agate.SpaceShooter
     public class Player : MonoBehaviour
     {
         [SerializeField] private float _moveSpeed = 4f;
+        [SerializeField] private float _moveMaxDistance = 4f;
+        [SerializeField] private float _shootDelay = 1f;
         [SerializeField] private PlayerBullet _bulletPrefab;
 
         private Rigidbody2D _rigidbody;
         private float _moveDirection;
+        private float _shootDelayCounter;
 
         private void Awake()
         {
@@ -18,7 +21,16 @@ namespace Agate.SpaceShooter
         private void FixedUpdate()
         {
             Vector2 moveTarget = _rigidbody.position + Vector2.right * (_moveDirection * _moveSpeed * Time.fixedDeltaTime);
+            moveTarget.x = Mathf.Clamp(moveTarget.x, -_moveMaxDistance, _moveMaxDistance);
+
             _rigidbody.MovePosition(moveTarget);
+
+            _shootDelayCounter += Time.fixedDeltaTime;
+            if (_shootDelayCounter > _shootDelay)
+            {
+                _shootDelayCounter = 0f;
+                PlayerBullet bullet = Instantiate(_bulletPrefab, transform.position, transform.rotation);
+            }
         }
 
         private void Update()
@@ -34,11 +46,6 @@ namespace Agate.SpaceShooter
             else
             {
                 _moveDirection = 0f;
-            }
-
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                PlayerBullet bullet = Instantiate(_bulletPrefab, transform.position, transform.rotation);
             }
         }
     }
--- Assets/Scripts/PlayerBullet.cs
+++ Assets/Scripts/PlayerBullet.cs
@@ -5,19 +5,24 @@ namespace Agate.SpaceShooter
     public class PlayerBullet : MonoBehaviour
     {
         [SerializeField] private float _speed = 8f;
+        [SerializeField] private float _maxDistance = 8f;
         
         private Rigidbody2D _rigidbody;
         
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
-            Destroy(gameObject, 2f);
         }
 
         private void FixedUpdate()
         {
             Vector2 moveTarget = _rigidbody.position + Vector2.up * (_speed * Time.fixedDeltaTime);
             _rigidbody.MovePosition(moveTarget);
+
+            if (transform.position.y > _maxDistance)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
